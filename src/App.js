3dde555333d7ffbb745ec59e0f2ffc335f1af1c3
drameBoubacar <boubacardrame9@gmail.com>
2019-02-18 10:17:35 +0000

    get data with componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import api from './lib/mock';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      product : []
+    }
+  }
+  componentDidMount(){
+    api.get()
+    .then(res => this.setState({
+      product : res
+    }))
+  }
   render() {
+    console.log('STATE', this.state.product)
     return (
       <div className="App">
-        
+
       </div>
     );
   }
